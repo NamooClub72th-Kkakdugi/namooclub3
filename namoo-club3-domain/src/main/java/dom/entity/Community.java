1edddcb1438e58f4134742b548277d54a3528f5a
kosta-18 <kosta-18@kosta-18-PC>
2014-04-08 08:59:11 +0900

    SocialPerson Community CommunityMember dom

--- namoo-club3-domain/src/main/java/dom/entity/Community.java
+++ namoo-club3-domain/src/main/java/dom/entity/Community.java
@@ -1,5 +1,86 @@
 package dom.entity;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Community {
+	
+	private int comNo;
+	private String comName;
+	private String description;
+	private Date openDate;
+	
+	private List<CommunityMember> members;
+	private List<Club> clubs;
+	
+	//----------------------------------------------------------------
+	public Community(int comNo, String comName, String description, SocialPerson user) {
+		//
+		this.comNo = comNo;
+		this.comName = comName;
+		this.description = description;
+		this.members = new ArrayList<CommunityMember>();
+		this.openDate = new Date();
+		
+		addMember(user);
+	}
+
+	
+	//-------------------------------------------------------------------
+	public int getComNo() {
+		return comNo;
+	}
+	
+	public void setComNo(int comNo) {
+		this.comNo = comNo;
+	}
+	
+	public String getComName() {
+		return comName;
+	}
+	
+	public void setComName(String comName) {
+		this.comName = comName;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public Date getOpenDate() {
+		return openDate;
+	}
+	
+	public void setOpenDate(Date openDate) {
+		this.openDate = openDate;
+	}
+	
+	public List<CommunityMember> getMembers() {
+		return members;
+	}
+	
+	public void setMembers(List<CommunityMember> members) {
+		this.members = members;
+	}
+	
+	public List<Club> getClubs() {
+		return clubs;
+	}
+	
+	public void setClubs(List<Club> clubs) {
+		this.clubs = clubs;
+	}
+//-----------------------------------------------------------------------------
+	public void addMember(SocialPerson rolePerson){
+		//
+		CommunityMember member = new CommunityMember(comName, rolePerson);
+		this.members.add(member);
+	}
+
 
 }
--- namoo-club3-domain/src/main/java/dom/entity/CommunityMember.java
+++ namoo-club3-domain/src/main/java/dom/entity/CommunityMember.java
@@ -2,4 +2,31 @@ package dom.entity;
 
 public class CommunityMember {
 
+	private String comName;
+	private SocialPerson user;
+	
+	//-----------------------------------------------------------------
+	public CommunityMember(String comName, SocialPerson user) {
+		//
+		this.comName = comName;
+		this.user = user;
+	}
+
+	//--------------------------------------------------------------------
+
+	public String getComName() {
+		return comName;
+	}
+
+	public void setComName(String comName) {
+		this.comName = comName;
+	}
+
+	public SocialPerson getUser() {
+		return user;
+	}
+
+	public void setUser(SocialPerson user) {
+		this.user = user;
+	}
 }
