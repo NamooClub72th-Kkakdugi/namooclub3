14eca4f3d176f723ce0ecc8fd39eb75d68f08fd4
kosta-19 <kosta-19@kosta-19-PC>
2014-04-08 09:15:08 +0900

    가져4

--- namoo-club3-domain/src/main/java/dom/entity/Club.java
+++ namoo-club3-domain/src/main/java/dom/entity/Club.java
@@ -1,5 +1,20 @@
 package dom.entity;
 
+import java.util.Date;
+import java.util.List;
+
 public class Club {
+	
+	private int comNo;
+	private int clubNo;
+	private int categoryNo;
+	private String clubName;
+	private String clubDes;
+	private Date openDate;
+	
+	private List<ClubMember> members;
+	
+	//---------------------------
+	public club(int comNo, int clubNo, int categoryNo, String clubName, String clubDes)
 
 }
