63a1334198bdab6a56ff5db6e37e882a58e10c4f
kosta-18 <kosta-18@kosta-18-PC>
2014-04-11 11:14:44 +0900

    aaaa

--- namoo-club3-web/src/main/java/com/namoo/club/web/controller/community/pres/PresCommunity.java
+++ namoo-club3-web/src/main/java/com/namoo/club/web/controller/community/pres/PresCommunity.java
@@ -1,10 +1,40 @@
 package com.namoo.club.web.controller.community.pres;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import dom.entity.CommunityManager;
+import dom.entity.CommunityMember;
+
 
 public class PresCommunity {
 	//
+	
+	private List<CommunityMember> members;
+	private CommunityManager manager;
+	
 	public PresCommunity() {
 		//
+		this.members = new ArrayList<CommunityMember>();
+		this.manager = manager;
 	}
+
+	public List<CommunityMember> getMembers() {
+		return members;
+	}
+
+	public void setMembers(List<CommunityMember> members) {
+		this.members = members;
+	}
+
+	public CommunityManager getManager() {
+		return manager;
+	}
+
+	public void setManager(CommunityManager manager) {
+		this.manager = manager;
+	}
+	
+	
 
 }
