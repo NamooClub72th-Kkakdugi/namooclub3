afb427e409d159bfc668337aa2206f930ce584a4
Juseong <wntjd1211@nate.com>
2014-04-21 12:12:14 +0900

    clubServiceTest

--- namoo-club3-service/src/test/java/com/namoo/club/service/facade/ClubServiceTest.java
+++ namoo-club3-service/src/test/java/com/namoo/club/service/facade/ClubServiceTest.java
@@ -1,6 +1,7 @@
 package com.namoo.club.service.facade;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import java.util.List;
 
@@ -56,21 +57,19 @@ public class ClubServiceTest extends DbCommonTest {
 		assertEquals(1, club.getCategoryNo());
 		assertEquals("club1", club.getName());
 		assertEquals("club1_des", club.getDescription());
-		
-		
-		
+		assertEquals("박상희", club.getKingManager().getName()); // 매니저 이름 확인
+		assertEquals(2, club.getManager().size()); // 매니저의 숫자 확인(매니저 + 킹매니저 = 2)
+		assertEquals(3, club.getMember().size()); //멤버 숫자 확인(멤버 + 매니저 + 킹매니저 = 3)
 	}
 	
 	@Test
 	public void testJoinAsMember() {
 		//
 		clubService.joinAsMember(4, "wntjd");
-		//
-		ClubMember member = clubService.findClubMember(4, "wntjd");
-		assertNotNull(member);
-		
-		
-		
+		Club club = clubService.findClub(4);
+		//검증
+		assertThat(club.getMember().size(), is(1));
+		assertThat(club.getMember().get(0).getEmail(), is("wntjd"));
 	}
 
 	@Test
@@ -79,6 +78,7 @@ public class ClubServiceTest extends DbCommonTest {
 		List<Club> clubs = clubService.findAllClubs(1);
 		//검증
 		assertEquals(4, clubs.size());
+		assertThat(clubs.get(1).getName(), is("club2"));
 	}
 
 	@Test
@@ -89,17 +89,18 @@ public class ClubServiceTest extends DbCommonTest {
 		// 검증
 		assertEquals(1, clubMember.getClubNo());
 		assertEquals("wntjd", clubMember.getEmail());
-//		System.out.println(clubMember.getType());
-//		assertEquals('c', clubMember.getType());
+		assertThat(clubMember.getName(), is("이주성"));
+//		assertThat(clubMember.getType(), is('c'));
 	}
 
 	@Test
 	public void testFindAllClubMember() {
 		//
-		int clubMembers = clubService.findAllClubMember(1).size();
+		List<ClubMember> clubMembers = clubService.findAllClubMember(1);
 		
 		// 검증 
-		assertEquals(3, clubMembers);
+		assertEquals(3, clubMembers.size());
+		assertThat(clubMembers.get(0).getName(), is("박상희"));
 	}
 
 	@Test
@@ -108,7 +109,10 @@ public class ClubServiceTest extends DbCommonTest {
 		clubService.removeClub(1, 1, true);
 		
 		// 검증
-		assertEquals(3, clubService.findAllClubs(1).size());
+		List<Club> clubs = clubService.findAllClubs(1);
+		assertEquals(3, clubs.size());
+		assertThat(clubs.get(0).getName(), is("club2"));
+		
 	}
 
 	@Test
@@ -118,6 +122,7 @@ public class ClubServiceTest extends DbCommonTest {
 		
 		// 검증
 		assertEquals(1, clubs.size());
+		assertThat(clubs.get(0).getName(), is("club1"));
 	}
 
 	@Test
@@ -127,6 +132,8 @@ public class ClubServiceTest extends DbCommonTest {
 	
 		//
 		assertEquals(1, managedClubs.size());
+		assertThat(managedClubs.get(0).getName(), is("club1"));
+		
 	}
 
 	@Test
@@ -135,22 +142,28 @@ public class ClubServiceTest extends DbCommonTest {
 		clubService.withdrawalClub(1, "wntjd");
 		//검증
 		assertEquals(2, clubService.findAllClubMember(1).size());
+		assertNull(clubService.findClubMember(1, "wntjd"));
 	}
 
 	@Test
-	public void testCommissionFromManagerCommunity() {
+	public void testCommissionManagerCommunity() {
 		//
 		clubService.commissionManagerClub(1, new SocialPerson("hong", "홍길동"), new SocialPerson("wntjd", "이주성"));
 		//
 		assertEquals(2, clubService.findAllClubManager(1).size());
+		assertThat(clubService.findClubManager(1, "wntjd").getName(), is("이주성"));
+		assertThat(clubService.findClubMember(1, "hong").getName(), is("홍길동"));
+		
+		
 	}
 	
 	@Test
-	public void testCommissionGoKingManagerClub() {
+	public void testCommissionKingManagerClub() {
 		//
 		clubService.commissionGoKingManagerClub(1, new SocialPerson("ekdgml", "박상희"), new SocialPerson("hong", "홍길동"));
-		
 		//
 		assertEquals("hong", clubService.findClubKingManager(1).getEmail());
+		assertThat(clubService.findClubKingManager(1).getName(), is("홍길동"));
+		assertThat(clubService.findClubManager(1, "ekdgml").getName(), is("박상희"));
 	}
 }
